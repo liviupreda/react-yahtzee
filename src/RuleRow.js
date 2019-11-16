529b907605b7f237fc757874b8a836828af52746
Liviu Preda <liviupreda@gmail.com>
2019-11-16 12:33:48 +0200

    modify RuleRow.js to show rule description if score is undefined

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,7 +3,7 @@ import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
-    const { score, name, doScore } = this.props;
+    const { score, name, doScore, description } = this.props;
     const undefScore = score === undefined;
     return (
       <tr
@@ -11,7 +11,7 @@ class RuleRow extends Component {
         onClick={undefScore ? doScore : null}
       >
         <td className="RuleRow-name">{name}</td>
-        <td className="RuleRow-score">{score}</td>
+        <td className="RuleRow-score">{undefScore ? description : score}</td>
       </tr>
     );
   }
