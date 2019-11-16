fb0aae19998ec952354350ee3472b3453f70e802
Liviu Preda <liviupreda@gmail.com>
2019-11-16 10:31:37 +0200

    refactor RuleRow

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,15 +3,15 @@ import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    const { score, name, doScore } = this.props;
+    const undefScore = score === undefined;
     return (
       <tr
-        className={`RuleRow RuleRow-${
-          this.props.score === undefined ? "active" : "disabled"
-        }`}
-        onClick={this.props.score === undefined ? this.props.doScore : null}
+        className={`RuleRow RuleRow-${undefScore ? "active" : "disabled"}`}
+        onClick={undefScore ? doScore : null}
       >
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
     );
   }
